7be0018afd5bb1beedeac45c0e90922d008c10f2
= <=>
2023-10-26 21:59:23 +0700

    Config migration

---
+++ src/TeduBlog.Data/TeduDBContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeduBlog.Data
+{
+    public class TeduDBContextFactory : IDesignTimeDbContextFactory<TeduDBContext>
+    {
+        public TeduDBContext CreateDbContext(string[] args)
+        {
+            var configuration = new ConfigurationBuilder()
+                 .SetBasePath(Directory.GetCurrentDirectory())
+                 .AddJsonFile("appsettings.json")
+                 .Build();
+            var builder = new DbContextOptionsBuilder<TeduDBContext>();
+            builder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            return new TeduDBContext(builder.Options);
+        }
+    }
+}
